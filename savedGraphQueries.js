5db8f64d6f2c0898aa685f3f1b1098a090465b01
Rio Edwards <rioredwards@gmail.com>
2023-04-17 16:57:43 -0700

    Clear out index.ts and prep for fetching project data

---
+++ savedGraphQueries.js
@@ -0,0 +1,17 @@
+// query projectEntryQuery {
+//   project(id: "5dNpDFILHCFxJXtXJrpsjZ") {
+//     sys {
+//       id
+//     }
+//     # add the fields you want to query
+//     title
+//     slug
+//     description
+//     image {
+//       title
+//       url
+//       width
+//       height
+//     }
+//   }
+// }
