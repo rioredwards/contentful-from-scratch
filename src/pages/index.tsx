b43ff28b76d6a867b8c314907a975f1f7769d7c5
Rio Edwards <rioredwards@gmail.com>
2023-04-17 17:17:57 -0700

    Fetch all projects from Contentful and display on screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,23 +8,56 @@ export default function Home({ projects }: { projects: Project[] }) {
   return (
     <main>
       <h1>Projects</h1>
-      <pre>{JSON.stringify(projects)}</pre>
+      <div className="space-y-4">
+        <pre className="w-96 whitespace-pre-wrap">
+          {JSON.stringify(projects)}
+        </pre>
+      </div>
     </main>
   );
 }
 
 export async function getStaticProps() {
   // Fetch data from Contentful
+  const response = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+          # add your query
+          projectCollection {
+            items {
+              title
+              description
+              image {
+                title
+                url
+                width
+              }
+            }
+          }
+        }
+        `,
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    console.error("Error Fetching Data: ", response);
+  }
+
+  const data = await response.json();
+  const projects = data.data.projectCollection.items;
 
   return {
     props: {
-      projects: [
-        {
-          title: "Project 1",
-          description: "Description 1",
-          image: "https://via.placeholder.com/150",
-        },
-      ],
+      projects: [projects],
     },
   };
 }
